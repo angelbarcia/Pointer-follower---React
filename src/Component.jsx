222b27c72fc38d36bbcc0d40e55fa02a9af461ef
Angel Barcia <angelbarcia2710@hotmail.com>
2025-01-15 12:17:29 +0100

    Initial commit

---
+++ src/Component.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+const Component = () => {
+  const [value, setValue] = useState(false);
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
+};
