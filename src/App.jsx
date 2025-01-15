54625f15c917a7a81670e5fa85f62869c53b4d85
Angel Barcia <angelbarcia2710@hotmail.com>
2025-01-15 13:14:53 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
 import { useState, useEffect } from "react";
 
-function App() {}
+function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("useEffect", { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  return (
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px,${position.y}px)`,
+        }}
+      ></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Deactivate" : "Activate"} follow pointer
+      </button>
+    </main>
+  );
+}
 
 export default App;
--- src/Component.jsx
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from "react";
-
-const Component = () => {
-  const [value, setValue] = useState(false);
-
-  useEffect(() => {
-    console.log("useEffect");
-  }, []);
-};
